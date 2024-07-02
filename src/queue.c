b1a828d8fa131aaae6baae0930fa85f98a024597
Ayanaksha Upadhyaya <uayanaksha@gmail.com>
2024-07-02 18:08:37 +0530

    dependency ds

---
+++ src/queue.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+#ifndef QUEUE_H
+#define QUEUE_H
+typedef struct Queue {
+    int *list;
+    int head, tail;
+    unsigned maxVol;
+} *Queue;
+
+Queue QueConstructor(unsigned maxVol){
+    if(maxVol < 1) return NULL;
+    Queue que = malloc(sizeof(Queue));
+    if(que){
+        que->list = (int*) calloc(maxVol, sizeof(int));
+        que->head = -1;
+        que->tail = -1;
+        que->maxVol = maxVol;
+    }   return que;
+}
+
+bool QueIsEmpty(Queue que){
+    return que->head == -1 && que->head == que->tail;
+}
+
+bool QueIsFull(Queue que){
+    return que->tail == que->maxVol -1;
+}
+
+void enqueue(Queue que, int data){
+    if(QueIsFull(que)) return;
+    if(QueIsEmpty(que)) que->head++;
+    que->list[++que->tail] = data;
+}
+
+int dequeue(Queue que){
+    int res =  QueIsEmpty(que) ? -1 : que->list[que->head++];
+    if(que->head > que->tail) {
+        que->head = -1;
+        que->tail = -1;
+    }   return res;
+}
+
+void QueDestructor(Queue que){   
+    free(que->list);
+    free(que);
+}
+#endif
---
+++ src/stack.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+#ifndef STACK_H
+#define STACK_H
+typedef struct Stack{
+    int *list;
+    int head;
+    unsigned maxVol;
+} *Stack;
+
+Stack StConstructor(unsigned maxVol){
+    if(maxVol < 1) return NULL;
+    Stack st = malloc(sizeof(Stack));
+    if(st){
+        st->list = (int*) calloc(maxVol, sizeof(int));
+        st->head = -1;
+        st->maxVol = maxVol;
+    }   return st;
+}
+
+bool StIsEmpty(Stack st){
+    return st->head == -1;
+}
+
+bool StIsFull(Stack st){
+    return st->head == st->maxVol -1;
+}
+
+void StPush(Stack st, int data){
+    if(StIsFull(st)) return;
+    st->list[++st->head] = data;
+}
+
+int StPop(Stack st){
+    int res =  StIsEmpty(st) ? -1 : st->list[st->head--];
+    if(st->head < 0) {
+        st->head = -1;
+    }   return res;
+}
+
+void StDestructor(Stack st){   
+    free(st->list);
+    free(st);
+}
+#endif
